86304b546f9747069d23df42db7e5219885cfd94
montanarice <davidrice363@gmail.com>
2019-07-30 13:37:35 -0600

    changed findAssemblies to return full path to each found assembly instead of the containing folder

--- src/main/java/qatch/utility/FileUtility.java
+++ src/main/java/qatch/utility/FileUtility.java
@@ -1,5 +1,6 @@
 package qatch.utility;
 
+import com.sun.istack.internal.NotNull;
 import com.sun.istack.internal.Nullable;
 
 import java.io.File;
@@ -17,46 +18,41 @@ public final class FileUtility {
 
     /**
      * Recursively find all files under a root directory with specified extension(s) and optionally with name matching
-     * a regex pattern
+     * a substring pattern
      *
      * @param rootDirectory
      *      Directory to begin recursive search at
      * @param name
-     *      File name a found file needs to at least partly match with (optional)
+     *      File name a found file needs to at least partly match with. "" matches everything.
      * @param extensions
      *      ex. ".exe", ".dll", "class" - The '.' is not necessary but is recommended
      * @return
-     *
+     *      The set of paths containing matched files
      */
-    public static Set<String> findAssemblies(File rootDirectory, @Nullable String name, String... extensions) {
+    public static Set<Path> findAssemblies(File rootDirectory, String name, @NotNull String... extensions) throws IllegalStateException {
 
         // bad approach for handling functional null Name string because I need to graduate soon
         if (name.isEmpty()) { name = ""; }
 
         Path root = Paths.get(rootDirectory.toString());
         ArrayList<String> exts = new ArrayList<>(Arrays.asList(extensions));
-        Set<String> assemblyPaths = new HashSet<>();
+        Set<Path> assemblyPaths = new HashSet<>();
 
-//        exts.forEach(ex -> {
-//            try {
-//                Files.find(root, Integer.MAX_VALUE, (path, attr) -> path.toString().endsWith(ex))
-//                        .forEach(path -> assemblyPaths.add(path.getParent().toString()));
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        });
         String finalName = name;
         exts.forEach(ex -> {
             try {
                 Files.find(root, Integer.MAX_VALUE, (path, attr) -> path.toString().endsWith(ex))
                         .filter(path -> path.toString().contains(finalName))
-                        .forEach(path -> assemblyPaths.add(path.getParent().toString()));
+                        .forEach(assemblyPaths::add);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
 
-
+        if (assemblyPaths.isEmpty()) {
+            throw new IllegalStateException("No assemblies with extension(s) " + Arrays.toString(extensions)
+                + " were found for scanning. Has the project been compiled?");
+        }
 
         return assemblyPaths;
     }
--- src/test/java/qatch/UtilityTests.java
+++ src/test/java/qatch/UtilityTests.java
@@ -1,11 +1,13 @@
 package qatch;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.junit.Test;
 import qatch.utility.FileUtility;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.Set;
 
 public class UtilityTests {
@@ -18,13 +20,45 @@ public class UtilityTests {
         File file2 = File.createTempFile("file2", ".ext1", new File("./src/test/output"));
         File file3 = File.createTempFile("file3", ".ext2", new File("./src/test/output"));
 
-        Set<String> assemblies = FileUtility.findAssemblies(new File("./src/test/output"), file1.getName().substring(2, 5), ".ext1");
+        Set<Path> ext1 = FileUtility.findAssemblies(new File("./src"), file1.getName().substring(0, 3), ".ext1");
+        Set<Path> ext2 = FileUtility.findAssemblies(new File("./src"), file1.getName().substring(0, 3), ".ext2");
+        Set<Path> extAll = FileUtility.findAssemblies(new File("./src"), file1.getName().substring(0, 3), ".ext2", ".ext1");
+        Set<Path> nullSubstring = FileUtility.findAssemblies(new File("./src"), "",  ".ext1");
+        Set<Path> substring = FileUtility.findAssemblies(new File("./src"), "file1", ".ext1");
+        Set<Path> fullSubstring = FileUtility.findAssemblies(new File("./src"), file2.getName(), ".ext1");
+
+        Assert.assertTrue(ext1.contains(file1.toPath()));
+        Assert.assertTrue(ext1.contains(file2.toPath()));
+        Assert.assertFalse(ext1.contains(file3.toPath()));
+
+        Assert.assertTrue(ext2.contains(file3.toPath()));
+        Assert.assertFalse(ext2.contains(file1.toPath()));
+        Assert.assertFalse(ext2.contains(file2.toPath()));
+
+        Assert.assertTrue(extAll.contains(file1.toPath()));
+        Assert.assertTrue(extAll.contains(file2.toPath()));
+        Assert.assertTrue(extAll.contains(file3.toPath()));
+
+        Assert.assertTrue(nullSubstring.contains(file1.toPath()));
+        Assert.assertTrue(nullSubstring.contains(file2.toPath()));
+        Assert.assertFalse(nullSubstring.contains(file3.toPath()));
+
+        Assert.assertTrue(substring.contains(file1.toPath()));
+        Assert.assertFalse(substring.contains(file2.toPath()));
+        Assert.assertFalse(substring.contains(file3.toPath()));
+
+        Assert.assertTrue(fullSubstring.contains(file2.toPath()));
+        Assert.assertFalse(fullSubstring.contains(file1.toPath()));
+        Assert.assertFalse(fullSubstring.contains(file3.toPath()));
+
+        boolean isEmpty = false;
+        try { Set<Path> noFoundExt = FileUtility.findAssemblies(new File("./src"), "", ".falseExt1", "falseExt2"); }
+        catch (IllegalStateException e) { isEmpty = true; }
+        finally { Assert.assertTrue(isEmpty); }
 
         file1.deleteOnExit();
         file2.deleteOnExit();
         file3.deleteOnExit();
-
-        System.out.println("waiting...");
     }
 
 }
