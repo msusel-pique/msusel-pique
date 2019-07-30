a8918fb7bd33956393716bfc5cca6a94fd059b60
David Rice <davidrice363@gmail.com>
2019-07-30 09:35:15 -0600

    temp commit workstation transfer

--- src/main/java/qatch/utility/FileUtility.java
+++ src/main/java/qatch/utility/FileUtility.java
@@ -13,7 +13,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public final class FileUtility {
-    private FileUtility() { }
+
 
     /**
      * Recursively find all files under a root directory with specified extension(s) and optionally with name matching
@@ -22,26 +22,41 @@ public final class FileUtility {
      * @param rootDirectory
      *      Directory to begin recursive search at
      * @param name
-     *      File name a found file needs to at least partly match with
+     *      File name a found file needs to at least partly match with (optional)
      * @param extensions
      *      ex. ".exe", ".dll", "class" - The '.' is not necessary but is recommended
      * @return
      *
      */
-    public static Set<String> findAssemblyDirectories(File rootDirectory, @Nullable String name, String... extensions) {
+    public static Set<String> findAssemblies(File rootDirectory, @Nullable String name, String... extensions) {
+
+        // bad approach for handling functional null Name string because I need to graduate soon
+        if (name.isEmpty()) { name = ""; }
 
         Path root = Paths.get(rootDirectory.toString());
         ArrayList<String> exts = new ArrayList<>(Arrays.asList(extensions));
         Set<String> assemblyPaths = new HashSet<>();
 
+//        exts.forEach(ex -> {
+//            try {
+//                Files.find(root, Integer.MAX_VALUE, (path, attr) -> path.toString().endsWith(ex))
+//                        .forEach(path -> assemblyPaths.add(path.getParent().toString()));
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        });
+        String finalName = name;
         exts.forEach(ex -> {
             try {
                 Files.find(root, Integer.MAX_VALUE, (path, attr) -> path.toString().endsWith(ex))
+                        .filter(path -> path.toString().contains(finalName))
                         .forEach(path -> assemblyPaths.add(path.getParent().toString()));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
+
+
 
         return assemblyPaths;
     }
--- src/test/java/qatch/UtilityTests.java
+++ src/test/java/qatch/UtilityTests.java
@@ -1,8 +1,30 @@
 package qatch;
 
-import org.junit.Assert;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
+import qatch.utility.FileUtility;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
 
 public class UtilityTests {
-    // TODO: add tests
+
+    @Test
+    public void testFindAssemblies() throws IOException {
+
+        FileUtils.forceMkdir(new File("./src/test/output"));
+        File file1 = File.createTempFile("file1", ".ext1", new File("./src/test/output"));
+        File file2 = File.createTempFile("file2", ".ext1", new File("./src/test/output"));
+        File file3 = File.createTempFile("file3", ".ext2", new File("./src/test/output"));
+
+        Set<String> assemblies = FileUtility.findAssemblies(new File("./src/test/output"), file1.getName().substring(2, 5), ".ext1");
+
+        file1.deleteOnExit();
+        file2.deleteOnExit();
+        file3.deleteOnExit();
+
+        System.out.println("waiting...");
+    }
+
 }
