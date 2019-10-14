81aaad0e814efead14a35851eb5c58ccaebb5b56
montanarice <davidrice363@gmail.com>
2019-10-14 15:14:13 -0600

    added yaml reader dependency to POM. Method header for Tool.mapMeasures()

--- src/main/java/qatch/analysis/Tool.java
+++ src/main/java/qatch/analysis/Tool.java
@@ -13,14 +13,57 @@ public abstract class Tool implements  ITool {
     private String name;
     private Set<HashMap<Measure, HashSet<Diagnostic>>> measureMappings = new HashSet<>();
 
-    // getters and setters
-    public String getName() {
-        return name;
+    // constructor
+
+    /**
+     * Constructor: on creation of any tool, the name of the tool should be specified
+     * and measure mapping set should be formed.
+     *
+     * @param name
+     *      The tool name
+     * @param toolConfig
+     *      The path to a config file (likely .yaml) describing which measure and associated rules
+     *      this tool will handle
+     */
+    public Tool(String name, Path toolConfig) {
+        this.name = name;
+        this.measureMappings = mapMeasures(toolConfig);
     }
+
+
+    // getters and setters
+    public String getName() { return name; }
     public Set<HashMap<Measure, HashSet<Diagnostic>>> getMeasureMappings() { return measureMappings; }
 
-    // methods
-    protected void mapMeasures(Path toolConfig) {
 
+    // methods
+    /**
+     * Read a .yaml config file that relates properties to their associated tool, measure, and diagnostics.
+     * The .yaml file should have the form
+     *      Property01Name:
+     *        Tool: Tool_Name
+     *        Measure: Measure_Name
+     *        Diagnostics:
+     *          - list
+     *          - of
+     *          - relevant
+     *          - diagnostic names
+     *      Security:
+     *        Tool: Roslynator
+     *        Diagnostics:
+     *          - SCS0001
+     *          - SCS0002
+     *
+     * @param toolConfig
+     *      Path location of the .yaml configuration
+     * @return
+     *      The object set represetnation of mapping of this tool's associated measures
+     *      and the diagnostics used to evaluate the measure
+     */
+    private Set<HashMap<Measure, HashSet<Diagnostic>>> mapMeasures(Path toolConfig) {
+
+
+
+        return null;
     }
 }
