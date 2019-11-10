1e1696eb413360bd7b2eb826bb104fdd149e8666
David Rice <davidrice363@gmail.com>
2019-11-10 16:28:18 -0700

    Added Calibrator skeleton class

---
+++ src/main/java/qatch/calibration/Calibrator.java
@@ -0,0 +1,12 @@
+package qatch.calibration;
+
+/**
+ * Utility class responible for running the calibration module's procedure.
+ * This uses a benchmark repository, quality model description, directory of comparison matrices,
+ * and the RInvoker as input to perform a 3 step process of
+ *     (1) Derive thresholds
+ *     (2) Elicitate weights
+ *     (3) Apply these results to the quality model to generate a fully derived quality model
+ */
+public class Calibrator {
+}
